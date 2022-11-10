e793b379b5afa52d32832ea138defd1c3853508c
Clarence Ho <ho.clarence13@gmail.com>
2022-11-10 17:05:23 +0800

    4th commit

--- main.js
+++ main.js
@@ -78,106 +78,12 @@ $('#start').on('click',()=>{
   })
 })
 
-//random color box
-/*
-let arr=['(255,0,0)','(255,128,0)','(255,255,0)','(128,255,0)','(0,0,255)','(127,0,255)','(255,0,255)']
 
-const rand2=()=>{
-  return arr[Math.floor(Math.random()*arr.length)];
-}
-console.log(rand2())
-
-$('#first-box').on('click', function(){
-  $(this).css('background-color')
-})
-
-$('#start').on('click',()=>{
-  $('#first-box').css({
-    'background-color':'rgb' + rand2()
-  })
-})
-
-$('#second-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#second-box').css({
-    'background-color':'rgb' + rand2() 
-  })
-})
-
-$('#third-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#third-box').css({
-    'background-color':'rgb' + rand2() 
-  })
-})
-
-$('#fourth-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#fourth-box').css({
-    'background-color':'rgb' + rand2() 
-  })
-})
-
-$('#fifth-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#fifth-box').css({
-    'background-color':'rgb' + rand2() 
-  })
-})
-
-$('#sixth-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#sixth-box').css({
-    'background-color':'rgb' + rand2()
-  })
-})
-
-$('#last-box').on('click', function(){
-  $(this).css('background-color') 
-})
-
-$('#start').on('click',()=>{
-  $('#last-box').css({
-    'background-color':'rgb' + rand2() 
-  })
-})
-*/
 //start and reset button turn red when mouse hover over them
 $('#start').on('mouseenter',()=>{
   $('#start').css('color','red');
 }).on('mouseleave',()=>{$('#start').css('color','black')});
 
-/*
-$('#generate').on('mouseenter',()=>{
-  $('#generate').css('color','red');
-}).on('mouseleave',()=>{$('#generate').css('color','black')}); */
-
-$('#reset').on('mouseenter',()=>{
-  $('#reset').css('color','red');
-}).on('mouseleave',()=>{$('#reset').css('color','black')});
-
-//function for reset button
-const reset=()=>{
-  $('#reset').on('click',()=>{
-    $('.square').empty()
-  })
-}
-reset();
 
 const variables =["Red","Orange","Yellow","Green","Blue","Purple","Pink"]
 arr=['#first-box','#second-box','#third-box','#fourth-box','#fifth-box','#sixth-box','#last-box'];
@@ -188,59 +94,76 @@ const randomWords=()=>{
 console.log(randomWords())
 
 
-/*
-const check =(i)=>{
-  if ($(arr[i]).text(randomWords())===variables[i]) {
-    $('#condition-box').text("Yes, you scored a point!");
-    } else if ($(arr[i]).text(randomWords())!==variables[i]) {
-      $('#condition-box').text("Sorry, please try again.");
-    }};
+const showAlert1=()=> {
+  alert("You Win!");
+}
+
+const showAlert2=()=> {
+  alert("Game Over!");
+}
+//showAlert();
+
+let winLose=()=>{
+let newVal=($('#box').text());
+if(newVal>= 20){
+  showAlert1();
+}else if(newVal<-20){
+  showAlert2();
+}
+}
+winLose();
+////timer!!!
+var timeLeft = 45;
+    var elem = document.getElementById('timer');
     
-
-console.log($(arr[0]).text(randomWords()).get());
-console.log(variables[0])
-
-const generateAll=()=>{
-  $('#start').on('click',()=>{
-    for(let i=0;i<arr.length;i++){
-     $(arr[i]).on('click',()=> check(i))
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == -1) {
+        clearTimeout(timerId);
+        showAlert2();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
       }
-    })
-  }
-generateAll(); 
-*/
-
-/*
-
-let point =0
-const $point=$('<div>').addClass("point").text(`Points ${point}`);
-*/
-
+    }
+////
+const gamePlay=()=>{
 const generateFirst=()=>{
   $('#start').on('click',(event)=>{
     $('#first-box').text(randomWords());
-    $('#first-box').on('click', ()=>{
+    countdown();
+    $('#first-box').on('click',()=>{
       if($('#first-box').text() === "Red"){
-       $('#condition-box').text("Yes,that is correct!");
-      $('#box').append(num())}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose(); };
         if($('#first-box').text() !== "Red"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();}
+      })
     })
-  })
-}
+  }
 generateFirst();
-
-
-
 
 const generateSecond=()=>{
   $('#start').on('click',(event)=>{
     $('#second-box').text(randomWords());
+    
     $('#second-box').on('click',()=>{
       if($('#second-box').text() === "Green"){
-        $('#condition-box').text("Yes,that is correct!")}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose();};
         if($('#second-box').text() !== "Green"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();}
       })
     })
   }
@@ -251,9 +174,15 @@ const generateThird=()=>{
     $('#third-box').text(randomWords());
     $('#third-box').on('click',()=>{
       if($('#third-box').text() === "Yellow"){
-        $('#condition-box').text("Yes,that is correct!")}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose();}
         if($('#third-box').text() !== "Yellow"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1)
+        winLose();}
       })
     })
   }
@@ -264,9 +193,15 @@ const generateFourth=()=>{
     $('#fourth-box').text(randomWords());
     $('#fourth-box').on('click',()=>{
       if($('#fourth-box').text() === "Orange"){
-        $('#condition-box').text("Yes,that is correct!")}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose();}
         if($('#fourth-box').text() !== "Orange"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();}
       })
     })
   }
@@ -277,9 +212,15 @@ const generateFifth=()=>{
     $('#fifth-box').text(randomWords());
     $('#fifth-box').on('click',()=>{
       if($('#fifth-box').text() === "Blue"){
-        $('#condition-box').text("Yes,that is correct!")}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose();}
         if($('#fifth-box').text() !== "Blue"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();}
       })
     })
   }
@@ -290,9 +231,15 @@ const generateSixth=()=>{
     $('#sixth-box').text(randomWords());
     $('#sixth-box').on('click',()=>{
       if($('#sixth-box').text() === "Pink"){
-        $('#condition-box').text("Yes,that is correct!")}
+        $('#condition-box').text("Yes,that is correct!")
+        let oldVal =parseInt($('#box').text());
+        $('#box').text(oldVal +1);
+      winLose();}
         if($('#sixth-box').text() !== "Pink"){
-         $('#condition-box').text("Sorry, please try again.")}
+         $('#condition-box').text("Sorry, please try again.");
+         let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();}
       })
     })
   }
@@ -303,68 +250,21 @@ const generateSixth=()=>{
         $('#last-box').text(randomWords());
         $('#last-box').on('click',()=>{
           if($('#last-box').text() === "Purple"){
-            $('#condition-box').text("Yes,that is correct!");}
+            $('#condition-box').text("Yes,that is correct!")
+            let oldVal =parseInt($('#box').text());
+            $('#box').text(oldVal +1);
+          winLose();}
             if($('#last-box').text() !== "Purple"){
-             $('#condition-box').text("Sorry, please try again.");}
+             $('#condition-box').text("Sorry, please try again.");
+             let oldVal =parseInt($('#box').text());
+         $('#box').text(oldVal -1);
+        winLose();};
+         
           })
         })
       }
-       generateLast();
+      generateLast();
+    }
+    gamePlay();
 
-
-
-/*
-const gameOver=()=>{
-  $('#start').on('click',()=>{
-    $('#condition-box').text("Game Over!")
-  })
-}
-console.log(gameOver());
-
-setTimeout(()=>{
-return gameOver;
-},2000);
-*/
-
-
-/*
-let score=0;
-
-$('.scorewrap').text(`Score:${score++}`)
-*/
-
-
-
-//make a settimeout function 
-//once startgame is clicked, condition-box has game over message in 5 seconds 
-
-/*
-const myTimeout=$('#start').on('click',()=>{
-  if ($('#first-box').on('click', ()=>{
-    if($('#first-box').text() === "Red"){
-      $('#condition-box').text("Yes,you scored a point!");}
-      if($('#first-box').text() !== "Red"){
-       $('#condition-box').text("Sorry, please try again.")}
-  })
-
-  setTimeout(myTimeout, 5000);
-
-  const timeOut=()=>{
-   $('#condition-box').text("Game Over!");
-  }
-
-  clearTimeout(myTimeout)
-
-
-
-
-
-/*
-$('#first-box').on('click',(event)=>{
-  console.log('hi');
-  if($('#first-box').text() === "Red"){
-   $('#condition-box').text("Yes")}
-   if($('#first-box').text() !== "Red"){
-    $('#condition-box').text("No")}
- }) */
-
+ 
